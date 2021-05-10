91e52b96a1d99d2e50088beaa8af4e6b6cc377fa
Eugene <Eugeniolio@yandex.com>
2021-05-10 13:32:44 +0300

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Евгений Фирман on 10.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
